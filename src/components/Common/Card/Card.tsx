3bd3e840aee1c40cf5d82f73a96da6ac4485b720
Egor <yatskovskiy2001@gmail.com>
2022-06-14 02:13:29 +0300

    Верстка компонета Card

--- src/components/Common/Card/Card.tsx
+++ src/components/Common/Card/Card.tsx
@@ -1,7 +1,8 @@
-import React, {FC, useRef} from "react";
-import "./Card.scss"
+import React, {FC, useRef, useState} from "react";
+import "./сard.scss"
 import {NavLink} from "react-router-dom";
 import {BASE_IMG_URL} from "../../../API/indexAPI";
+import styles from "./card.module.scss"
 import {useTypedSelector} from "../../../hooks/useTypedSelector";
 import {ICard} from "../../../models/cardM";
 import {getCardClassType} from "../../../helpers/getCardClassType";
@@ -9,8 +10,34 @@ import {useFavorite} from "../../../hooks/useFavorite";
 import * as mainPageSelectors from "../../../store/selectors/mainPageSelectors";
 import {useWatchList} from "../../../hooks/useWatchList";
 import {ProfileLinksNames} from "../../../models/ProfileM";
+import {IconButton, SvgIcon, Tooltip} from "@mui/material";
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import FavoriteBorderTwoToneIcon from '@mui/icons-material/FavoriteBorderTwoTone';
+import BookmarkBorderTwoToneIcon from '@mui/icons-material/BookmarkBorderTwoTone';
+import FormatListBulletedTwoToneIcon from '@mui/icons-material/FormatListBulletedTwoTone';
 
-const Card: FC<ICard> = ({title,
+import {createTheme} from '@mui/material/styles';
+
+
+const theme = createTheme({
+    status: {
+        danger: '#e53e3e',
+    },
+    palette: {
+        primary: {
+            main: '#0971f1',
+            // @ts-ignore
+            darker: '#053e85',
+        },
+        neutral: {
+            main: '#64748B',
+            contrastText: '#fff',
+        },
+    },
+});
+
+const Card: FC<ICard> = ({
+                             title,
                              bg_path,
                              vote,
                              type,
@@ -18,8 +45,9 @@ const Card: FC<ICard> = ({title,
                              overview,
                              genres,
                              typeAPI,
-                             listType= null,
-                             date}) => {
+                             listType = null,
+                             date
+                         }) => {
 
     const genresTv = useTypedSelector(mainPageSelectors.genresTv)
     const genresMovie = useTypedSelector(mainPageSelectors.genresMovie)
@@ -29,52 +57,81 @@ const Card: FC<ICard> = ({title,
 
     const classType = getCardClassType()
 
-    const addFavourite = (e:React.MouseEvent<Element, MouseEvent>):void => {
+    const addFavourite = (e: React.MouseEvent<Element, MouseEvent>): void => {
         e.preventDefault()
         deleteOrSaveCard(ProfileLinksNames.favorites)
-        setFavorite({itemId:id, isFavorite:true})
+        setFavorite({itemId: id, isFavorite: true})
     }
 
-    const addWatchList = (e:React.MouseEvent<Element, MouseEvent>):void => {
+    const addWatchList = (e: React.MouseEvent<Element, MouseEvent>): void => {
         e.preventDefault()
         deleteOrSaveCard(ProfileLinksNames.watchlist)
-        setWatchList({itemId:id, isToWatchList:true})
+        setWatchList({itemId: id, isToWatchList: true})
     }
 
-    function deleteOrSaveCard (type:string) {
-        if ( listType === type && cardRef !== null) {
+    function deleteOrSaveCard(type: string) {
+        if (listType === type && cardRef !== null) {
             (cardRef.current as HTMLAnchorElement).style.display = "none"
         }
     }
 
-
+    let [isHover, setHover] = useState(false)
+    let mainGenres = typeAPI === "movie" ?
+        `${genresMovie?.find(item => genres && item.id === genres[0])?.name},
+                            ${genresMovie?.find(item => genres && item.id === genres[1])?.name || ""}` :
+        genresTv?.find(item => genres && item.id === genres[0])?.name
     return (
-        <NavLink ref={cardRef} className={`${classType[type].card} `} to={`/${typeAPI}/${id} `}>
-            <div className={`${classType[type].card__header}`}>
-                <img src={`${BASE_IMG_URL}${bg_path}`} alt=""/>
-            </div>
-            <div className={`${classType[type].card__main} `}>
-                <div className={`${classType[type].card__titleBox}`}>
-                    <h1 className={`${classType[type].card__title}`}>{title}</h1>
-                    <div className={`${classType[type].card__date}`}>{date}</div>
-                    <div className={`${classType[type].card__vote}`}>{vote}</div>
+        <NavLink to={`/${typeAPI}/${id} `} ref={cardRef}  onMouseOver={() => setHover(true)} onMouseLeave={() => setHover(false)} className={styles.card}>
+            <div className={styles.card_box}>
+                <div className={styles.card_img}>
+                    {bg_path && <img src={`${BASE_IMG_URL}${bg_path}`} alt=""/>}
                 </div>
 
-                <p className={`${classType[type].card__overview}`}>{overview}</p>
-                <div className={`${classType[type].card__btns}`}>
-                    <button data-tooltip = "Оценить">&#9734;<span>оценить</span></button>
-                    <button  onClick={(e) => addFavourite(e)} data-tooltip = "В избранное">&#9829;<span>избранное</span></button>
-                    <button data-tooltip = "Добавить в список">&#65049;<span>в список</span></button>
-                    <button onClick={(e) => addWatchList(e)} data-tooltip = "В закладки">&#1349;<span>позже</span></button>
-                    <button>del<span>удалить</span></button>
-                </div>
+                {isHover && <div className={styles.hover}>
+                  <div className={styles.buttons_box}>
+                    <Tooltip title={"Оценить"} placement={"right"}>
+                      <div className={styles.button}>
+                        <IconButton color={'default'} size={"small"}>
+                          <SvgIcon sx={{fontSize: 15}} component={StarBorderOutlinedIcon} inheritViewBox/>
+                        </IconButton>
+                      </div>
 
-                <div className={`${classType[type].card__genres}`}>{
-                    typeAPI === "movie" ?
-                        genresMovie?.find( item => genres && item.id === genres[0])?.name :
-                        genresTv?.find( item => genres && item.id === genres[0])?.name}</div>
+                    </Tooltip>
+                    <Tooltip title={"В избранное"} placement={"right"}>
+                      <div onClick={(e) => addFavourite(e)} className={styles.button}>
+                        <IconButton color={'default'} size={"small"}>
+                          <SvgIcon sx={{fontSize: 15}} component={FavoriteBorderTwoToneIcon} inheritViewBox/>
+                        </IconButton>
+                      </div>
+                    </Tooltip>
+                    <Tooltip title={"Добавить в список"} placement={"right"}>
+                      <div className={styles.button}>
+                        <IconButton color={'default'} size={"small"}>
+                          <SvgIcon sx={{fontSize: 15}} component={FormatListBulletedTwoToneIcon} inheritViewBox/>
+                        </IconButton>
+                      </div>
+                    </Tooltip>
+                    <Tooltip title={"В закладки"} placement={"right"}>
+                      <div onClick={(e) => addWatchList(e)} className={styles.button}>
+                        <IconButton color={'default'} size={"small"}>
+                          <SvgIcon sx={{fontSize: 15}} component={BookmarkBorderTwoToneIcon} inheritViewBox/>
+                        </IconButton>
+                      </div>
+                    </Tooltip>
+                  </div>
+                  <div>
+                    <div className={styles.vote}>{vote}</div>
+                    <div className={styles.genres}>{mainGenres}</div>
+                    <div className={styles.date}>{date}</div>
+                  </div>
+                </div>}
+
+                <div className={styles.card_title_box}>
+                    <h2>{title}</h2>
+                </div>
             </div>
         </NavLink>
+
     )
 }
 
